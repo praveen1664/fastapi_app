4442d57e8b59a88d7f0db99808bf57247fe3d9f6
Praveen <36715149+praveen1664@users.noreply.github.com>
2022-01-04 19:24:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def tree():
     return {"myFavouritrTree":"Banyan Tree"}
 
 def main():
-    uvicorn.run(app, port=8080, host='0.0.0.0')
+    uvicorn.run(app, port=80, host='0.0.0.0')
 
 if __name__ == '__main__':
     main()
